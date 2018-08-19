dbabbab290e3196f336ee71763481a4a5eb8b53f
Chishin <pro@xxgzs.org>
2018-08-19 15:14:38 +0800

    添加资源文件输出功能

--- ListFileHelper.cs
+++ ListFileHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace ResistanceSelectionTool
@@ -45,6 +46,37 @@ namespace ResistanceSelectionTool
                         dic.Add(arrData[0], arrData[1]);
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// 输出资源文件
+        /// </summary>
+        /// <param name="path">输出文件路径</param>
+        /// <param name="res">资源文件位置“命令空间.文件夹.文件全名”</param>
+        /// <param name="isReplace">是否替换已存在文件</param>
+        public static void OutputResFile(string path, string res, bool isReplace = false)
+        {
+            if (File.Exists(path))
+            {
+                if (isReplace)
+                {
+                    File.Delete(path);
+                }
+                else
+                {
+                    Console.WriteLine("资源文件已存在，取消释放文件。");
+                    return;
+                }
+            }
+
+            Assembly assm = Assembly.GetExecutingAssembly();
+            Stream istr = assm.GetManifestResourceStream(res);
+            StreamReader sr = new StreamReader(istr);
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                sw.Write(sr.ReadToEnd());
+                Console.WriteLine("资源文件释放完成。");
             }
         }
     }
