50df0f9e0b18a643e366caa01923fdfe1dbc437e
Chishin <pro@xxgzs.org>
2018-11-10 21:45:49 +0800

    增加升压模块电阻计算类框架

---
+++ ResBoostMode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ResistanceSelectionTool
+{
+    public class ResBoostMode
+    {
+
+        #region 字段
+        private Thread ThreadResBoostModeProcess;
+        private bool isRun = false;
+        #endregion
+
+        #region 属性
+        public double VolFB { get; set; }
+        public double VolOut { get; set; }
+        public double VolPWM { get; set; }
+        public double[] ArrayResData { get; set; }
+        #endregion
+
+        #region 构造函数
+        public ResBoostMode(double[] data, double Vfb = 0.6, double Vout = 9, double Vpwm = 0)
+        {
+            ArrayResData = data;
+            VolFB = Vfb;
+            VolOut = Vout;
+            VolPWM = Vpwm;
+            ThreadResBoostModeProcess = new Thread(new ThreadStart(ThreadResBoostModeProcess_Event));
+            ThreadResBoostModeProcess.IsBackground = true;
+        }
+        #endregion
+
+        #region 事件
+        /// <summary>
+        /// 电阻计算结果事件委托
+        /// </summary>
+        /// <param name="status">状态</param>
+        /// <param name="message">消息内容</param>
+        /// <param name="value">完成进度/阻值结果</param>
+        public delegate void DelegateResBoostModeReturn(EnumResCalcStatus status, string message, double percent, double[] value);
+
+        /// <summary>
+        /// 电阻计算结果事件
+        /// </summary>
+        public event DelegateResBoostModeReturn EventResBoostModeReturn;
+        #endregion
+
+        #region 公共函数
+        public void Start()
+        {
+            isRun = true;
+            ThreadResBoostModeProcess.Start();
+        }
+
+        public void Stop()
+        {
+            isRun = false;
+        }
+        #endregion
+
+        #region 计算处理函数
+        public void ThreadResBoostModeProcess_Event()
+        {
+
+        }
+        #endregion
+    }
+
+    public enum EnumResBoostModeStatus
+    {
+        Start,
+        Run,
+        Done,
+        Wait,
+        Error
+    }
+}
